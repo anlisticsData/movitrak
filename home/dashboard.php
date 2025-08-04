9878d3baba784205dad37ac2a8b9510160b6b613
edi <analisticsdataprojetos@gmail.com>
2025-08-04 10:43:21 -0300

    painel

--- home/dashboard.php
+++ home/dashboard.php
@@ -308,6 +308,7 @@ $dias_em_portugues = array_map(function ($dia) use ($dias_da_semana) {
                                 <tbody>
                     `;
                     response.data.forEach(function(movimento) {
+                        movimento = movimento.movimentos
                         const imageUrl = movimento.file_path ? `../${movimento.file_path}` : '../assets/img/no-image.png';
                         const placaText = movimento.placa ? movimento.placa : 'N/A';
                         const createdAtFormatted = new Date(movimento.created_at).toLocaleString('pt-BR');
