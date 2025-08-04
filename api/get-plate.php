d429e4808d5363ee90650877a64202609c40aa6b
edi <analisticsdataprojetos@gmail.com>
2025-08-04 10:28:42 -0300

    painel

--- api/get-plate.php
+++ api/get-plate.php
@@ -11,7 +11,27 @@ try {
         $userId = $_POST['user'];
         $plate =  $_POST['plate'] ?? '';
         $movimentVacanciesDAO =  new MovimentVacanciesDAO($pdo);
-        $lancamentos = $movimentVacanciesDAO->getTodosMovimentosDoDiaAtual($plate, $userId);
+        $lancamentosData = $movimentVacanciesDAO->getTodosMovimentosDoDiaAtual($plate, $userId);
+
+        $lancamentos=[];
+        foreach($lancamentosData as $key => $row){
+            $lancamentos[]=[
+                "dia"=>$row['dia'],
+                "movimento_id"=>$row['movimento_id'],
+                "fk_camera"=> $row['fk_camera'],
+                "fk_vacancie"=> $row['fk_vacancie'],
+                "state"=>$row['state'],
+                "file_path"=> $row['file_path'],
+                "placa"=>$row['placa'],
+                "created_at"=> $row['created_at']
+            ];
+        }
+
+
+
+
+
+
         echo json_encode([
             "success" => true,
             "data" =>    $lancamentos
