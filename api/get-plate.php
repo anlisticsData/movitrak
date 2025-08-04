b10dc1537ff39f9b7174f36cc3ed24eb36f8922a
edi <analisticsdataprojetos@gmail.com>
2025-08-04 10:31:00 -0300

    painel

--- api/get-plate.php
+++ api/get-plate.php
@@ -5,6 +5,24 @@ header('Content-Type: application/json');
 
 
 
+/**
+ * Extrai uma placa no padrão Mercosul (ex: ABC1D23) de uma string.
+ *
+ * @param string $string A string de onde extrair a placa.
+ * @return string|null A placa extraída ou null se não for encontrada.
+ */
+function extrairPlacaMercosul(string $string): ?string
+{
+    if (preg_match('/[A-Z]{3}[0-9][A-Z0-9][0-9]{2}/', $string, $matches)) {
+        return $matches[0];
+    }
+
+    return "desconhecia";
+}
+
+
+
+
 try {
     $lancamentos = [];
     if (isset($_POST['user'])) {
@@ -22,7 +40,7 @@ try {
                 "fk_vacancie"=> $row['fk_vacancie'],
                 "state"=>$row['state'],
                 "file_path"=> $row['file_path'],
-                "placa"=>$row['placa'],
+                "placa"=>extrairPlacaMercosul($row['placa']),
                 "created_at"=> $row['created_at']
             ];
         }
