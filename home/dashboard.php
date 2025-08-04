d2081b1748c9b17816417bca74fc05bc0882a905
edi <analisticsdataprojetos@gmail.com>
2025-08-04 10:51:30 -0300

    painel

--- home/dashboard.php
+++ home/dashboard.php
@@ -289,7 +289,7 @@ $dias_em_portugues = array_map(function ($dia) use ($dias_da_semana) {
                 buscarBtn.innerHTML = originalButtonHtml;
                 buscarBtn.disabled = false;
 
-                console.log("##4", response.data)
+                console.log("##4",response.data)
 
                 $('#searchResultsBody').empty();
                 if (response.success && response.data && response.data.length > 0) {
@@ -307,14 +307,11 @@ $dias_em_portugues = array_map(function ($dia) use ($dias_da_semana) {
                                 </thead>
                                 <tbody>
                     `;
-                    response.data.forEach(function(row) {
-
-
-                        row.movimentos.forEach((movimento) => {
-                            const imageUrl = movimento.file_path ? `../${movimento.file_path}` : '../assets/img/no-image.png';
-                            const placaText = movimento.placa ? movimento.placa : 'N/A';
-                            const createdAtFormatted = new Date(movimento.created_at).toLocaleString('pt-BR');
-                            tableHtml += `
+                    response.data.forEach(function(movimento) {
+                        const imageUrl = movimento.file_path ? `../${movimento.file_path}` : '../assets/img/no-image.png';
+                        const placaText = movimento.placa ? movimento.placa : 'N/A';
+                        const createdAtFormatted = new Date(movimento.created_at).toLocaleString('pt-BR');
+                        tableHtml += `
                             <tr>
                                 <td>${movimento.fk_vacancie}</td>
                                 <td>${placaText}</td>
@@ -325,12 +322,6 @@ $dias_em_portugues = array_map(function ($dia) use ($dias_da_semana) {
                                 <td></td>
                             </tr>
                         `;
-
-                        })
-
-
-
-
                     });
                     tableHtml += `
                                 </tbody>
